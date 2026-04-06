810734c6b94c8d4a06d6932780896ce32dd801bd
최재원 <anjsk11@naver.com>
2026-04-06 12:47:26 +0900

    Initial Commit: Spring Boot 4.0.5 기반 기초 설정

---
+++ src/main/java/me/anjsk/bestoffer/BestOfferApplication.java
@@ -0,0 +1,13 @@
+package me.anjsk.bestoffer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BestOfferApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BestOfferApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/anjsk/bestoffer/BestOfferApplicationTests.java
@@ -0,0 +1,13 @@
+package me.anjsk.bestoffer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BestOfferApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
