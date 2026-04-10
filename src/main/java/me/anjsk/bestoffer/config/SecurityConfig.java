36f96343fee2da09916e7631d78813804f32672c
최재원 <anjsk11@naver.com>
2026-04-10 20:15:30 +0900

    fix: disable csrf

--- src/main/java/me/anjsk/bestoffer/config/SecurityConfig.java
+++ src/main/java/me/anjsk/bestoffer/config/SecurityConfig.java
@@ -2,10 +2,28 @@ package me.anjsk.bestoffer.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                // CSRF 방어 끄기
+                .csrf(csrf -> csrf.disable())
+
+                // 시큐리티는 모든 경로의 접근을 허용 (문단속은 인터셉터가 할 예정)
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().permitAll()
+                );
+
+        return http.build();
+    }
+
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
