19e3c7f704343447a0454fd325145c79dc8e8e8d
최재원 <anjsk11@naver.com>
2026-04-12 01:03:46 +0900

    feat: cancelled pessimistic lock

--- src/main/java/me/anjsk/bestoffer/repository/AuctionRepository.java
+++ src/main/java/me/anjsk/bestoffer/repository/AuctionRepository.java
@@ -15,9 +15,4 @@ import java.util.Optional;
 public interface AuctionRepository extends JpaRepository<Auction,Long> {
     // ON_SALE 상태이고, 마감 시간이 현재보다 이전인 경매 찾기
     List<Auction> findByStatusAndEndTimeBefore(AuctionStatus status, LocalDateTime time);
-
-    // 기존 findById 대신 사용할 비관적 락 전용 메서드!
-    @Lock(LockModeType.PESSIMISTIC_WRITE)
-    @Query("SELECT a FROM Auction a WHERE a.id = :id")
-    Optional<Auction> findByIdWithPessimisticLock(@Param("id") Long id);
 }
