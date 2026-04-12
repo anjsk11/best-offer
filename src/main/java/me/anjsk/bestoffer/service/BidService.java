d441fbef0a733a82cfd1eb045f078e78f3fbc62e
최재원 <anjsk11@naver.com>
2026-04-12 14:06:19 +0900

    feat: changed to pessimistic lock

--- src/main/java/me/anjsk/bestoffer/service/BidService.java
+++ src/main/java/me/anjsk/bestoffer/service/BidService.java
@@ -29,17 +29,13 @@ public class BidService {
 
     public Long placeBid(Long auctionId, Long bidPrice, Long bidderId, LocalDateTime bidTime) {
 
-        Auction auction = auctionRepository.findById(auctionId)
+        Auction auction = auctionRepository.findByIdWithPessimisticLock(auctionId)
                 .orElseThrow(AuctionNotFoundException::new);
         User bidder = userRepository.findById(bidderId)
                 .orElseThrow(UserNotFoundException::new);
 
         // 비즈니스 로직 검증 및 현재가 갱신
         auction.updateCurrentPrice(bidPrice, bidder, bidTime);
-
-        // 강제 쓰기(Flush): 변경된 Auction 정보를 DB에 즉시 반영
-        // 이 메서드가 끝나고 Facade에서 락을 풀었을 때, 다음 사람이 최신 가격을 보도록
-        auctionRepository.saveAndFlush(auction);
 
         // 입찰 기록(Bid) 생성 및 저장
         Bid bid = new Bid(bidPrice, auction, bidder, bidTime);
